ae492faf11d47f1577accef8b4eeefa1369d120c
Jakub Misiaszek <jmisiaszek3@gmail.com>
2026-01-05 01:26:37 +0100

    Inference criterion option

--- trajectory_inference.sh
+++ trajectory_inference.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 if [ $# -ne 2 ]; then
-    echo "Usage: $0 <output_prefix> <test_prefix>"
+    echo "Usage: $0 <output_prefix> <test_prefix> <scoring criterion: BDE | MDL>"
     exit 1
 fi
 
 output_prefix="$1"
 test_prefix="$2"
+criterion="$3"
 
 mkdir -p inference
 
@@ -14,6 +15,6 @@ find ./datasets -type f -name "${test_prefix}_bn_*_trajectories.txt" | while rea
     if [[ "$file" =~ ${test_prefix}_bn_([0-9]+)_trajectories\.txt$ ]]; then
         num="${BASH_REMATCH[1]}"
         output_file="inference/${output_prefix}_${num}.sif"
-        bnf -e "$file" -n "$output_file" -v
+        bnf -e "$file" -n "$output_file" -v -s "$criterion"
     fi
 done
