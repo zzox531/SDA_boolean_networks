711582d896386a44b6e7a20fb8269c285af6a537
Jakub Misiaszek <jmisiaszek3@gmail.com>
2026-01-05 01:35:16 +0100

    Ups

--- perform_tests.sh
+++ perform_tests.sh
@@ -4,9 +4,9 @@ set -e
 
 # configs: "fr_lo fr_hi len_lo len_hi sync_no async_no test_prefix seed"
 configs=(
-    "1 1 10 50 50 50 test0 42"
-    "1 1 10 50 0 50 test1 42"
-    "1 1 10 50 50 0 test2 42"
+    "1 1 10 50 50 50 test0 42 BDE"
+    "1 1 10 50 0 50 test1 42 BDE"
+    "1 1 10 50 50 0 test2 42 BDE"
 )
 BN_JSON="datasets/boolean_networks.json"
 
@@ -16,6 +16,7 @@ for conf in "${configs[@]}"; do
     set -- $conf
     fr_lo=$1; fr_hi=$2; len_lo=$3; len_hi=$4
     sync_no=$5; async_no=$6; test_prefix=$7; seed=$8
+    criterion=$9
 
     tg_json="datasets/${test_prefix}_trajectory_samples.json"
     log_file="logs/traj_gen_${test_prefix}.log"
@@ -36,7 +37,7 @@ for conf in "${configs[@]}"; do
         -s $seed
 
     echo "=== Running inference for $test_prefix ==="
-    ./trajectory_inference.sh "$test_prefix" "$test_prefix"
+    ./trajectory_inference.sh "$test_prefix" "$test_prefix" "$criterion"
 
     echo "--- Completed $test_prefix ---"
     echo
--- trajectory_inference.sh
+++ trajectory_inference.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ $# -ne 2 ]; then
+if [ $# -ne 3 ]; then
     echo "Usage: $0 <output_prefix> <test_prefix> <scoring criterion: BDE | MDL>"
     exit 1
 fi
