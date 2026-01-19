ea92d404d21c955b5181d6f1f134e63d33126570
Piotr Maksymiuk <p.maksymiuk2@student.uw.edu.pl>
2026-01-19 01:37:32 +0100

    More configs

--- perform_tests.sh
+++ perform_tests.sh
@@ -4,8 +4,50 @@ set -e
 
 # configs: "ratio_lo ratio_hi fr_lo fr_hi len_lo len_hi sync_no async_no test_prefix seed criterion"
 configs=(
-    "0.2 0.4 1 1 10 50 100 0 test0 42 BDE"
-    "0.2 0.4 1 1 10 50 100 0 test1 42 BDE"
+    "0.4 0.6 1 1  25  25 100 0    len-025-sync 42"
+    "0.4 0.6 1 1  50  50 100 0    len-050-sync 42"
+    "0.4 0.6 1 1  75  75 100 0    len-075-sync 42"
+    "0.4 0.6 1 1 100 100 100 0    len-100-sync 42"
+    "0.4 0.6 1 1 125 125 100 0    len-125-sync 42"
+    "0.4 0.6 1 1 150 150 100 0    len-150-sync 42"
+    "0.4 0.6 1 1 175 175 100 0    len-175-sync 42"
+    "0.4 0.6 1 1 200 200 100 0    len-200-sync 42"
+    "0.4 0.6 1 1 225 225 100 0    len-225-sync 42"
+    "0.4 0.6 1 1 250 250 100 0    len-250-sync 42"
+    "0.4 0.6 1 1 275 275 100 0    len-275-sync 42"
+    "0.4 0.6 1 1 300 300 100 0    len-300-sync 42"
+    "0.0 0.1 1 1 50 300  100 0  ratio-0.1-sync 42"
+    "0.1 0.2 1 1 50 300  100 0  ratio-0.2-sync 42"
+    "0.2 0.3 1 1 50 300  100 0  ratio-0.3-sync 42"
+    "0.3 0.4 1 1 50 300  100 0  ratio-0.4-sync 42"
+    "0.4 0.5 1 1 50 300  100 0  ratio-0.5-sync 42"
+    "0.5 0.6 1 1 50 300  100 0  ratio-0.6-sync 42"
+    "0.6 0.7 1 1 50 300  100 0  ratio-0.7-sync 42"
+    "0.7 0.8 1 1 50 300  100 0  ratio-0.8-sync 42"
+    "0.8 0.9 1 1 50 300  100 0  ratio-0.9-sync 42"
+    "0.9 1.0 1 1 50 300  100 0  ratio-1.0-sync 42"
+    "0.4 0.6 1 1  25  25 0 100   len-025-async 42"
+    "0.4 0.6 1 1  50  50 0 100   len-050-async 42"
+    "0.4 0.6 1 1  75  75 0 100   len-075-async 42"
+    "0.4 0.6 1 1 100 100 0 100   len-100-async 42"
+    "0.4 0.6 1 1 125 125 0 100   len-125-async 42"
+    "0.4 0.6 1 1 150 150 0 100   len-150-async 42"
+    "0.4 0.6 1 1 175 175 0 100   len-175-async 42"
+    "0.4 0.6 1 1 200 200 0 100   len-200-async 42"
+    "0.4 0.6 1 1 225 225 0 100   len-225-async 42"
+    "0.4 0.6 1 1 250 250 0 100   len-250-async 42"
+    "0.4 0.6 1 1 275 275 0 100   len-275-async 42"
+    "0.4 0.6 1 1 300 300 0 100   len-300-async 42"
+    "0.0 0.1 1 1 50 300  0 100 ratio-0.1-async 42"
+    "0.1 0.2 1 1 50 300  0 100 ratio-0.2-async 42"
+    "0.2 0.3 1 1 50 300  0 100 ratio-0.3-async 42"
+    "0.3 0.4 1 1 50 300  0 100 ratio-0.4-async 42"
+    "0.4 0.5 1 1 50 300  0 100 ratio-0.5-async 42"
+    "0.5 0.6 1 1 50 300  0 100 ratio-0.6-async 42"
+    "0.6 0.7 1 1 50 300  0 100 ratio-0.7-async 42"
+    "0.7 0.8 1 1 50 300  0 100 ratio-0.8-async 42"
+    "0.8 0.9 1 1 50 300  0 100 ratio-0.9-async 42"
+    "0.9 1.0 1 1 50 300  0 100 ratio-1.0-async 42"
 )
 
 BN_PREF="datasets/bn_"
@@ -16,7 +58,6 @@ for conf in "${configs[@]}"; do
     set -- $conf
     ratio_lo=$1; ratio_hi=$2; fr_lo=$3; fr_hi=$4; len_lo=$5; len_hi=$6
     sync_no=$7; async_no=$8; test_prefix=$9; seed=${10}
-    criterion=${11}
 
     tg_json="datasets/${test_prefix}_trajs_bn_"
     log_file="logs/traj_gen_${test_prefix}.log"
@@ -39,7 +80,8 @@ for conf in "${configs[@]}"; do
         -s $seed
 
     echo "=== Running inference for $test_prefix ==="
-    ./trajectory_inference.sh "$test_prefix" "$test_prefix" "$criterion"
+    ./trajectory_inference.sh "${test_prefix}_BDE" "$test_prefix" "BDE"
+    ./trajectory_inference.sh "${test_prefix}_MDL" "$test_prefix" "MDL"
 
     echo "--- Completed $test_prefix ---"
     echo
