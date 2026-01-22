362077e2b1e24f46c5336a796ea13d5a96a0694d
zzox531 <ignacykozakiewicz@gmail.com>
2026-01-22 14:19:45 +0100

    Implement chicken inference script, separate chicken dataset from regular datasets

---
+++ chicken_test.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+set -e
+
+BN_PREF="chicken_dataset/bn_"
+
+mkdir -p logs chicken_inference
+
+ratio_lo=0.25
+ratio_hi=0.4
+fr_lo=1
+fr_hi=1
+len_lo=25
+len_hi=25
+sync_no=10
+async_no=0
+seed=42
+
+tg_json="chicken_dataset/chicken_bn_"
+log_file="logs/chicken.log"
+
+echo "=== Generating trajectories for chicken test ==="
+python3 trajectory_generator.py \
+    -ratio-lo $ratio_lo \
+    -ratio-hi $ratio_hi \
+    -fr-lo $fr_lo \
+    -fr-hi $fr_hi \
+    -len-lo $len_lo \
+    -len-hi $len_hi \
+    -sync-no $sync_no \
+    -async-no $async_no \
+    -bn-ds "$BN_PREF" \
+    -tg-ds "$tg_json" \
+    -tg-ds-txt "chicken_dataset/chicken" \
+    -lf "$log_file" \
+    -s $seed
+
+echo "=== Running inference for chicken test ==="
+
+mkdir -p chicken_inference
+mkdir -p chicken_inference/sif
+mkdir -p chicken_inference/cpd
+
+output_file_BDE_sif="chicken_inference/sif/chicken_test-BDE-sync-0.sif"
+output_file_BDE_cpd="chicken_inference/cpd/chicken_test-BDE-sync-0.cpd"
+
+output_file_MDL_sif="chicken_inference/sif/chicken_test-MDL-sync-0.sif"
+output_file_MDL_sif="chicken_inference/cpd/chicken_test-MDL-sync-0.cpd"
+
+bnf -e "chicken_dataset/chicken_bn_0_trajectories.txt" -n "$output_file_BDE_sif" -c "$output_file_BDE_cpd" -v -s "BDE" -g -l 3 -k 16
+bnf -e "chicken_dataset/chicken_bn_0_trajectories.txt" -n "$output_file_MDL_sif" -c "$output_file_MDL_cpd" -v -s "MDL" -g -l 3 -k 16
+
+echo "=== CHICKEN INFERENCE COMPLETE ==="
--- trajectory_inference.sh
+++ trajectory_inference.sh
@@ -10,7 +10,6 @@ test_prefix="$2"
 criterion="$3"
 
 mkdir -p inference
-mkdir -p inference/bif
 mkdir -p inference/sif
 mkdir -p inference/cpd
 
@@ -18,8 +17,7 @@ find ./datasets -type f -name "${test_prefix}_bn_*_trajectories.txt" | while rea
     if [[ "$file" =~ ${test_prefix}_bn_([0-9]+)_trajectories\.txt$ ]]; then
         num="${BASH_REMATCH[1]}"
         output_file_sif="inference/sif/${output_prefix}-${num}.sif"
-        output_file_bif="inference/bif/${output_prefix}-${num}.bif"
         output_file_cpd="inference/cpd/${output_prefix}-${num}.cpd"
-        bnf -e "$file" -n "$output_file_sif" -b "$output_file_bif" -c "$output_file_cpd" -v -s "$criterion" -g -l 3 -k 16
+        bnf -e "$file" -n "$output_file_sif" -c "$output_file_cpd" -v -s "$criterion" -g -l 3 -k 16
     fi
 done
